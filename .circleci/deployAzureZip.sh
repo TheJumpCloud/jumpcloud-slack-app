416babdef05cc619087ec67787af46842b1fda18
Joe Workman <joe.workman@jumpcloud.com>
2021-05-25 13:18:18 -0600

    comments

--- .circleci/deployAzureZip.sh
+++ .circleci/deployAzureZip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script zips the contents of the ../Azure Directory
+# This script zips the contents of the ../Azure Directory & uploads to the s3Uri bucket
 # Currently the 'sa-circle-ci' repository contains the JumpCloudAllForSlackAzure.zip
 s3Uri='s3://sa-circle-ci'
 echo "Present Working Directory:"
@@ -8,6 +8,9 @@ echo "Setting Current Working Directory to script location..."
 cd "$(dirname "$0")" || exit
 echo "Present Working Directory:"
 pwd
+
+# zip Azure Directory
 zip -r JumpCloudAppForSlackAzure.zip ../Azure
 
+# Upload to s3Uri
 aws s3 cp ./JumpCloudAppForSlackAzure.zip $s3Uri
