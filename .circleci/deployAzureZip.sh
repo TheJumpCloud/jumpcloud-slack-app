4dbe9d50ffe7ce7f79a20d51494f370326c915e0
Joe Workman <joe.workman@jumpcloud.com>
2021-05-25 13:15:20 -0600

    set cwd to script location

--- .circleci/deployAzureZip.sh
+++ .circleci/deployAzureZip.sh
@@ -2,7 +2,12 @@
 # This script zips the contents of the ../Azure Directory
 # Currently the 'sa-circle-ci' repository contains the JumpCloudAllForSlackAzure.zip
 s3Uri='s3://sa-circle-ci'
-
+echo "Present Working Directory:"
+pwd
+echo "Setting Current Working Directory to script location..."
+cd "$(dirname "$0")" || exit
+echo "Present Working Directory:"
+pwd
 zip -r JumpCloudAppForSlackAzure.zip ../Azure
 
 aws s3 cp ./JumpCloudAppForSlackAzure.zip $s3Uri
