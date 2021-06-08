4c09b2856ede0e9443136c23ab169bedfcdead79
Joe Workman <joe.workman@jumpcloud.com>
2021-06-08 10:59:14 -0600

    Add Pester Files

---
+++ Deploy/InvokePester.ps1
@@ -0,0 +1,41 @@
+$PesterTests = Get-ChildItem -Path:($PSScriptRoot + '/*.Tests.ps1') -Recurse
+
+$PesterResultsFileXmldir = "$PSScriptRoot/test_results/"
+# $PesterResultsFileXml = $PesterResultsFileXmldir + "results.xml"
+if (-not (Test-Path $PesterResultsFileXmldir))
+{
+    new-item -path $PesterResultsFileXmldir -ItemType Directory
+}
+
+$configuration = [PesterConfiguration]::Default
+$configuration.Run.Path = "$PSScriptRoot"
+$configuration.Should.ErrorAction = 'Continue'
+$configuration.CodeCoverage.Enabled = $true
+$configuration.testresult.Enabled = $true
+$configuration.testresult.OutputFormat = 'JUnitXml'
+$configuration.Filter.Tag = $IncludeTags
+$configuration.Filter.ExcludeTag = $ExcludeTagList
+$configuration.CodeCoverage.OutputPath = ($PesterResultsFileXmldir + 'coverage.xml')
+$configuration.testresult.OutputPath = ($PesterResultsFileXmldir + 'results.xml')
+
+Write-Host ("[RUN COMMAND] Invoke-Pester -Path:('$PSScriptRoot') -TagFilter:('$($IncludeTags -join "','")') -ExcludeTagFilter:('$($ExcludeTagList -join "','")') -PassThru") -BackgroundColor:('Black') -ForegroundColor:('Magenta')
+Invoke-Pester -configuration $configuration
+
+$PesterTestResultPath = (Get-ChildItem -Path:("$($PesterResultsFileXmldir)")).FullName | Where-Object { $_ -match "results.xml" }
+If (Test-Path -Path:($PesterTestResultPath))
+{
+    [xml]$PesterResults = Get-Content -Path:($PesterTestResultPath)
+    If ($PesterResults.ChildNodes.failures -gt 0)
+    {
+        Write-Error ("Test Failures: $($PesterResults.ChildNodes.failures)")
+    }
+    If ($PesterResults.ChildNodes.errors -gt 0)
+    {
+        Write-Error ("Test Errors: $($PesterResults.ChildNodes.errors)")
+    }
+}
+Else
+{
+    Write-Error ("Unable to find file path: $PesterTestResultPath")
+}
+Write-Host -ForegroundColor Green '-------------Done-------------'
---
+++ Deploy/Tests/PSScriptAnalyzer.Tests.ps1
@@ -0,0 +1,48 @@
+################################################################################
+# Rules Skipped:
+# 'PSAvoidUsingWriteHost',
+# 'PSUseDeclaredVarsMoreThanAssignments',
+# 'PSReviewUnusedParameter',
+# 'PSAvoidUsingInvokeExpression'
+################################################################################
+
+Describe 'PSScriptAnalyzer Test Suite' {
+    Context 'PSScriptAnalyzer Tests'{
+        BeforeAll{
+            ### SET UP ###
+            $FolderPath_Module = (Get-Item -Path("$PSScriptRoot/../../")).FullName
+            $SettingsFile = "$PSScriptRoot/PSScriptAnalyzerSettings.psd1"
+            # Import Settings:
+            $SettingsFromFile = Import-PowerShellDataFile $SettingsFile
+            $settingsObject = @{
+                Severity     = $SettingsFromFile.Severity
+                ExcludeRules = $SettingsFromFile.ExcludeRules
+            }
+            ##############
+            Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
+            Write-Host ('[status]PSScriptAnalyzer Settings File: ' + $SettingsFile)
+            $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:("$FolderPath_Module") -recurse -Settings $settingsObject -reportSummary
+            If (-not [System.String]::IsNullOrEmpty($ScriptAnalyzerResults))
+            {
+                $ScriptAnalyzerResults | ForEach-Object {
+                    Write-Error ('[PSScriptAnalyzer][' + $_.Severity + '][' + $_.RuleName + '] ' + $_.Message + ' found in "' + $_.ScriptPath + '" at line ' + $_.Line + ':' + $_.Column)
+                }
+            }
+            Else
+            {
+                Write-Host ('[success]ScriptAnalyzer returned no results')
+            }
+        }
+        It 'PSScriptAnalyzer Results should be null'{
+            $ScriptAnalyzerResults | Should -BeNullOrEmpty
+        }
+        It 'PSScriptAnalyzer SettingsFile should exist' {
+            test-path $SettingsFile | Should -Be $true
+        }
+        It 'PSScriptAnalyzer SettingsObject Should Not Be Null or Empty'{
+            $SettingsFromFile | Should -Not -BeNullOrEmpty
+            $settingsObject | Should -Not -BeNullOrEmpty
+        }
+
+    }
+}
